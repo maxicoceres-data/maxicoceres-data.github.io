65e59d4bca6d04a5018fac4b2658c016065a6942
Maximiliano Coceres <maximiliano.g.coceres@gmail.com>
2026-03-17 12:34:16 +0100

    Refactor path_ubicacion function to improve clarity and maintainability

--- function/path.py
+++ function/path.py
@@ -1,10 +1,17 @@
 from pathlib import Path
 
 def path_ubicacion():
-    #conectar o crear base de datos
-    BASE_DIR = Path(__file__).resolve().parent.parent
-    DATA_FOLDER = BASE_DIR / "data"
-    DB_PATH = DATA_FOLDER / "proyectos.db"
-    DATA_FOLDER.mkdir(parents=True, exist_ok=True)
+    # 1. Ubicación de este archivo (path.py)
+    current_file = Path(__file__).resolve() 
+    
+    # 2. Subir a la raíz del proyecto (PORTFOLIO_ANALISIS)
+    # path.py está en /function, así que el padre es /function y el abuelo es la raíz.
+    BASE_DIR = current_file.parent.parent
+    
+    # 3. Apuntar a la carpeta data que está en la raíz
+    DB_PATH = BASE_DIR / "data" / "proyectos.db"
+    
+    # Aseguramos que la carpeta exista (por si acaso)
+    DB_PATH.parent.mkdir(parents=True, exist_ok=True)
     
     return DB_PATH
