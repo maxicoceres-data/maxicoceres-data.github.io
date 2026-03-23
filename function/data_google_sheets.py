072049d393750c10c0e17fcc587a1ed65e6e0445
maxicoceres-data <maximiliano.g.coceres@gmail.com>
2026-03-23 13:57:50 +0100

    Fix credential loading method in Google Sheets integration

--- function/data_google_sheets.py
+++ function/data_google_sheets.py
@@ -5,7 +5,7 @@ import streamlit as st
 def leer_Datos_google_sheets():
     ruta_json = st.secrets["GOOGLE_SHEETS"]
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name(ruta_json, scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(ruta_json, scope)
     client = gspread.authorize(creds)
     
     sheet = client.open("Datos portfolio").sheet1
